8cf32c4b592b3bd4fc3417b9920a8e40d8a61d4d
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-19 21:19:50 +0530

    Added header and footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Layout.module.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Layout({ title, keywords, description, children }) {
   return (
@@ -9,7 +11,9 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
