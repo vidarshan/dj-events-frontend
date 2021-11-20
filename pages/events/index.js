618d52b9fadd035173941922729162c47e4943de
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-20 14:03:38 +0530

    Get Static methods and events routes

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,27 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
+  console.log(events);
+
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Upcoming events</h1>
+      {events.length === 0 && <h3>No Events</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
