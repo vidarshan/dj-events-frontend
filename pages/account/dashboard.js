a5d63326aa1142f29c42e7c51dd37d64f773af65
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-21 22:44:02 +0530

    dashboard events edit and delete

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,12 +1,22 @@
 import Layout from "@/components/Layout";
+import DashboardEvent from "@/components/DashboardEvent";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/Dashboard.module.css";
 import { parseCookies } from "@/helpers/index";
 
 export default function DashboardPage({ events }) {
-  console.log({ events });
+  const deleteEvent = (id) => {
+    console.log(id);
+  };
+
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h3>My events</h3>
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
     </Layout>
   );
 }
