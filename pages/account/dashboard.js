78ca3ae984128b69ae4144f0ef08579260599ed9
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-21 22:32:50 +0530

    dashboard events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,31 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import { parseCookies } from "@/helpers/index";
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+  console.log({ events });
   return (
     <Layout title="User Dashboard">
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
